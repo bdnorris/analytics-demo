1a5dd6516e000a1480d5f67e8c079408307eb1fc
Brian Norris <bdnorris@gmail.com>
2019-07-05 19:14:51 -0500

    basics for demo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-/* Anything here will be processed by the javascript interpreter when this file finishes loading. */
+var trackEvent = function(category, action, label, value) {
+  console.log(category, action, label, value)
+  
+  if (typeof dataLayer != 'undefined') {
+    console.log('dataLayer', dataLayer);
+
+    dataLayer.push({
+      // ...
+    })
+
+    console.log('dataLayer', dataLayer);
+  }
+}
 
 
+document.querySelector('[data-button]').addEventListener('click', function(e) {
+  trackEvent('my category', 'click', e.target.innerHTML, 1);
+})
 
-$(document).ready(function(){
-/* Anything here will run when the entire HTML document has been processed by the browser */
-
-
-});
